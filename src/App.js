75adc5ba2cb356f65fb62664678598f5fde12540
guillaumegl <glebeau.dev@gmail.com>
2018-01-15 22:20:09 +0100

    41. Manipulating the State
    Section 3, Lecture 41

--- src/App.js
+++ src/App.js
@@ -8,11 +8,20 @@ class App extends Component {
       { name: 'Max', age: 28 },
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 }
-    ]
+    ],
+    otherState: 'some other value'
   };
 
   switchNameHandler = () => {
-    console.log('Was clicked!');
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    });
   };
 
   render() {
