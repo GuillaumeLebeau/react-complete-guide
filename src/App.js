453480b2d8cb64f300402777ac5befc934aa8612
guillaumegl <glebeau.dev@gmail.com>
2018-01-15 20:44:46 +0100

    JSX Restrictions
    Section 3, Lecture 30

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
       </div>
     );
     // return React.createElement(
