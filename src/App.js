217d9f7f1015403a493dbbd92e99d15ddf2899f5
guillaumegl <glebeau.dev@gmail.com>
2018-01-15 21:47:52 +0100

    Working with Components & Re-Using Them
    Section 3, Lecture 33

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ class App extends Component {
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         <Person />
+        <Person />
+        <Person />
       </div>
     );
     // return React.createElement(
