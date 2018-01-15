24fb5e1a533fe00f722eed120af574313e968820
guillaumegl <glebeau.dev@gmail.com>
2018-01-15 21:51:51 +0100

    34. Outputting Dynamic Content
    Section 3, Lecture 34

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const person = () => {
-  return <p>I'm a Person!</p>;
+  return (
+    <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
+  );
 };
 
 export default person;
