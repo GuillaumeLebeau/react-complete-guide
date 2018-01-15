688efa3b7f1e26dd1bda3382d7586baeb7a45841
guillaumegl <glebeau.dev@gmail.com>
2018-01-15 21:57:06 +0100

    35. Working with Props
    Section 3, Lecture 35

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Max" age="28" />
+        <Person name="Manu" age="29">
+          My Hobbies: Racing
+        </Person>
+        <Person name="Stephanie" age="26" />
       </div>
     );
     // return React.createElement(
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-const person = () => {
+const person = props => {
   return (
-    <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
+    <p>
+      I'm {props.name} and I am {props.age} years old!
+    </p>
   );
 };
 
