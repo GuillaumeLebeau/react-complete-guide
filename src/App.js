07fcdccd857a0f730f4127208ea139da206dd3cd
Guillaume Lebeau <guillaume.lebeau@irec.fr>
2018-01-19 17:03:20 +0100

    64. Using Radium for Media Queries
    Section 5, Lecture 64

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium from 'radium';
+import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -93,14 +93,16 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button style={style} onClick={this.togglePersonsHandler}>
-          Toggle Persons
-        </button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button style={style} onClick={this.togglePersonsHandler}>
+            Toggle Persons
+          </button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
     // return React.createElement(
     //   "div",
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,8 +4,13 @@ import Radium from 'radium';
 import './Person.css';
 
 const person = props => {
+  const style = {
+    '@media (min-width: 500px)': {
+      width: '450px'
+    }
+  };
   return (
-    <div className="Person">
+    <div className="Person" style={style}>
       <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old!
       </p>
