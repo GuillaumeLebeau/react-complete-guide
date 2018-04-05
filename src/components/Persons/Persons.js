8f71058ac491a3d921064adfd42b4a5202af72ea
guillaumegl <glebeau.dev@gmail.com>
2018-04-05 23:06:33 +0200

    87. Component Updating Lifecycle in Action
    Section 7, Lecture 87

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -16,6 +16,34 @@ class Persons extends Component {
     console.log('[Persons.js] Inside componentDidMount()');
   }
 
+  componentWillReceiveProps(nextProps) {
+    console.log(
+      '[UPDATE Persons.js] Inside componentWillReceiveProps',
+      nextProps
+    );
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log(
+      '[UPDATE Persons.js] Inside shouldComponentUpdate',
+      nextProps,
+      nextState
+    );
+    return nextProps.persons !== this.props.persons;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log(
+      '[UPDATE Persons.js] Inside componentWillUpdate',
+      nextProps,
+      nextState
+    );
+  }
+
+  componentDidUpdate() {
+    console.log('[UPDATE Persons.js] Inside componentDidUpdate');
+  }
+
   render() {
     console.log('[Persons.js] Inside render()');
     return this.props.persons.map((person, index) => {
