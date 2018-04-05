f2a6051795fcbd297092a2bb882aef7f81a2c319
guillaumegl <glebeau.dev@gmail.com>
2018-04-05 21:42:45 +0200

    79. A Better Project Structure
    Section 7, Lecture 79

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,11 +3,6 @@ import React from 'react';
 import classes from './Person.css';
 
 const person = props => {
-  const rnd = Math.random();
-  if (rnd > 0.7) {
-    throw new Error('Something went wrong');
-  }
-
   return (
     <div className={classes.Person}>
       <p onClick={props.click}>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+import Person from '../components/Persons/Person/Person';
 
 class App extends Component {
   state = {
@@ -54,14 +53,13 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <ErrorBoundary key={person.id}>
-                <Person
-                  click={() => this.deletePersonHandler(index)}
-                  name={person.name}
-                  age={person.age}
-                  changed={event => this.nameChangedHandler(event, person.id)}
-                />
-              </ErrorBoundary>
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={event => this.nameChangedHandler(event, person.id)}
+              />
             );
           })}
         </div>
