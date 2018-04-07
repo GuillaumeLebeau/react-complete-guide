4e993cd8a2c32895f55804d9090d570a1a0cbe96
guillaumegl <glebeau.dev@gmail.com>
2018-04-07 20:52:57 +0200

    89. Performance Gains with PureComponents
    Section 7, Lecture 89

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
 import Person from './Person/Person';
 
-class Persons extends Component {
+class Persons extends PureComponent {
   constructor(props) {
     super(props);
     console.log('[Persons.js] Inside Constructor', props);
@@ -23,14 +23,19 @@ class Persons extends Component {
     );
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log(
-      '[UPDATE Persons.js] Inside shouldComponentUpdate',
-      nextProps,
-      nextState
-    );
-    return nextProps.persons !== this.props.persons;
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log(
+  //     '[UPDATE Persons.js] Inside shouldComponentUpdate',
+  //     nextProps,
+  //     nextState
+  //   );
+  //   return (
+  //     nextProps.persons !== this.props.persons ||
+  //     nextProps.changed !== this.props.changed ||
+  //     nextProps.clicked !== this.props.clicked
+  //   );
+  //   // return true;
+  // }
 
   componentWillUpdate(nextProps, nextState) {
     console.log(
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
-class App extends Component {
+class App extends PureComponent {
   constructor(props) {
     super(props);
     console.log('[App.js] Inside Constructor', props);
@@ -31,14 +31,17 @@ class App extends Component {
     console.log('[App.js] Inside componentDidMount()');
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log(
-      '[UPDATE App.js] Inside shouldComponentUpdate',
-      nextProps,
-      nextState
-    );
-    return true;
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log(
+  //     '[UPDATE App.js] Inside shouldComponentUpdate',
+  //     nextProps,
+  //     nextState
+  //   );
+  //   return (
+  //     nextState.persons !== this.state.persons ||
+  //     nextState.showPersons !== this.state.showPersons
+  //   );
+  // }
 
   componentWillUpdate(nextProps, nextState) {
     console.log(
@@ -110,6 +113,13 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
+        <button
+          onClick={() => {
+            this.setState({ showPersons: true });
+          }}
+        >
+          Show Persons
+        </button>
         <Cockpit
           appTitle={this.props.title}
           showPersons={this.state.showPersons}
